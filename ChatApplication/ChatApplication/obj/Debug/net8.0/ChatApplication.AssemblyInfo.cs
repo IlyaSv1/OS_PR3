71ea96cbb149bdeb5a6ddc660b6d5d740fc0502b
IlyaSv1 <ilya.sv7490@gmail.com>
2026-03-22 15:26:18 +0500

    heartbeat

--- ChatApplication/ChatApplication/obj/Debug/net8.0/ChatApplication.AssemblyInfo.cs
+++ ChatApplication/ChatApplication/obj/Debug/net8.0/ChatApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6438a5cdb227e29fed9f7c325b8f4109bf41e437")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb774e023a8a36ad32c42063f496edaf1cdcfcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
